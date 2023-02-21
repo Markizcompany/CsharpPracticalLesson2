250732663aaa1fecdb603bf3b6843b337a0da676
Ruslan Kacheniuk <markizcompany@mail.ru>
2023-02-21 10:23:43 +0300

    Added task10

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 100 && number < 1000)
+{
+    int newnumber = SecNumber(number);
+    Console.WriteLine($"{newnumber}");
+}
+else Console.WriteLine("Вы ввели не трехзначное число");
+
+int SecNumber (int num)
+{
+    int firstNum = num % 100;
+    int secondNum = firstNum / 10;
+    return secondNum;
+}
