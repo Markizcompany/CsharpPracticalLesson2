00f959f5174d1cf5f61cc233ba35f85bee16e87b
Ruslan Kacheniuk <markizcompany@mail.ru>
2023-02-21 11:38:58 +0300

    Added task00 - task08

---
+++ task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+
+Console.WriteLine("Введите первое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (FirstNumber == SecondNumber * SecondNumber) Console.WriteLine($"{FirstNumber} квадрат {SecondNumber}");
+else Console.WriteLine($"{FirstNumber} не квадрат {SecondNumber}");
---
+++ task08/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+double number = Convert.ToDouble(Console.ReadLine());
+if (number > 0)
+    if (number % 1 == 0)
+    {
+        int count = 2;
+        while (count <= number)
+        {
+            Console.Write($"{count} ");
+            count = count + 2;
+        }
+    }
+    else Console.WriteLine("Введено неверное число. Введите натуральное число");
+else Console.WriteLine("Введено неверное число. Введите натуральное число");
