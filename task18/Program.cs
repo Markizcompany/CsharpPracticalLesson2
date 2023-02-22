06eeeda83dfa5d9eb7a6cd7985726fde72777cc1
Ruslan Kacheniuk <markizcompany@mail.ru>
2023-02-22 21:59:53 +0300

    Added task18

---
+++ task18/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 18. Напишите программу, которая 
+// по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+//    Y
+//  2 | 1
+// ---|---X
+//  3 | 4
+
+
+// методы
+string Prompt(string message)
+{
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+string Range(string num)
+{
+    if (num == "1") return "x > 0,  y > 0";
+    if (num == "2") return "x < 0,  y > 0";
+    if (num == "3") return "x < 0,  y < 0";
+    if (num == "4") return "x > 0,  y < 0";
+    return "0";
+}
+
+
+// тело программы
+string number = Prompt("Введите номер четверти ");
+string range = Range(number);
+string result = range=="0" ?
+                "Некорректный ввод данных"
+                : $"диапазон возможных координат точек в этой четверти {range}";
+Console.WriteLine(result);
