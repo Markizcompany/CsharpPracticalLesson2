f6df13e3b06ceeabdf1f0e58c7795a0d423bfac9
Ruslan Kacheniuk <markizcompany@mail.ru>
2023-03-01 22:53:48 +0300

    Added task26

---
+++ task26/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 26. Напишите программу, которая
+// принимает на вход число и
+// выдаёт количество цифр в числе.
+// 465 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int number = Prompt("Введите число: ");
+int digit = CheckDigit(number);
+Console.WriteLine($"{number} -> {digit}");
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int CheckDigit(int num)
+{
+    int result = 0;
+    if (num == 0) return 1;
+    else
+    {
+        while (num != 0)
+        {
+            num = num / 10;
+            result = result + 1;
+        }
+    }
+    return result;
+}
