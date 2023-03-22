b2b51e56deed1d65e057f795454f2df8f6afa713
Ruslan Kacheniuk <markizcompany@mail.ru>
2023-03-22 23:33:14 +0300

    Added task59

---
+++ task59/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 59.
+// Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец,
+// на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент -1, на выходе получим следующий массив:
+// 9 2 3
+// 4 2 4
+// 2 6 7
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];  // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  // или rows  вместо matrix.GetLength(0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // colums
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[] IndexMinElem(int[,] matrix)
+{
+    int row = 0;
+    int colum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= matrix[row, colum])
+            {
+                row = i;
+                colum = j;
+            }
+        }
+    }
+    return new int[] { row, colum };
+}
+
+
+int[,] DeleteRowColum(int[,] matrix, int[] array)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int m = 0, n = 0;  // m - строка,  n - столбец
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        if (m == array[0]) m++;
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            if (n == array[1]) n++;
+            newMatrix[i, j] = matrix[m, n];
+            n++;
+        }
+        m++;
+        n = 0;
+    }
+    return newMatrix;
+}
+
+
+int[,] array2d = CreateMatrixRndInt(3, 3, -10, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int[] indexMinElem = IndexMinElem(array2d);
+int[,] newArray2d = DeleteRowColum(array2d, indexMinElem);
+PrintMatrix(newArray2d);
